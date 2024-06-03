8218a586d54e8646fd7af59f01c763db698735fc
vguibert@amiltone.com <vguibert@amiltone.com>
2024-06-03 17:00:45 +0200

    first install

---
+++ src/main/java/com/perso/ClientJDR/ClientJdrApplication.java
@@ -0,0 +1,13 @@
+package com.perso.ClientJDR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientJdrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientJdrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perso/ClientJDR/ClientJdrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perso.ClientJDR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientJdrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
