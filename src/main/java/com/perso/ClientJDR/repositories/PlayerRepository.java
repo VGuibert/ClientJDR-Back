e68f7cf0b22815aba0fbbb04c72944b0b93144af
vguibert@amiltone.com <vguibert@amiltone.com>
2024-07-06 20:46:20 +0200

    replacing liquibase by hibernate ( temp ) + rename user by Player

--- src/main/java/com/perso/ClientJDR/repositories/PlayerRepository.java
+++ src/main/java/com/perso/ClientJDR/repositories/PlayerRepository.java
@@ -1,11 +1,11 @@
 package com.perso.ClientJDR.repositories;
 
 import com.perso.ClientJDR.config.jpa.ClientJdrRepository;
-import com.perso.ClientJDR.entities.User;
+import com.perso.ClientJDR.entities.Player;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends ClientJdrRepository<User, UUID> {
+public interface PlayerRepository extends ClientJdrRepository<Player, UUID> {
 }
